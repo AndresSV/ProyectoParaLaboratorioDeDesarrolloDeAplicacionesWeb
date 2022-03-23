3dc56e3fbfddff7a5ae96261d1881abdf94fe9c8
Vargas1723 <vargas23.bv@gmail.com>
2022-03-22 18:56:17 -0600

    Add/Gett user & report

--- proyecto/api/server.js
+++ proyecto/api/server.js
@@ -1,49 +1,101 @@
-var express = require('express');
-var mysql = require('mysql');
+var express = require("express")
+var mysql = require("mysql")
 
-var app = express();
+var app = express()
 
 var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'ReportMe'
-});
+  host: "localhost",
+  user: "root",
+  database: "ReportMe",
+})
 
-con.connect(function(err){
-    if (err) throw err;
-    console.log("Connection established.");
-});
+con.connect(function (err) {
+  if (err) throw err
+  console.log("Connection established.")
+})
 
 //Habilitando la aplicación para parsear json (POST request)
 app.use(express.json())
 
-//Creando un endpoint básico
-app.get('/', (req, res)=>{
-    let sql_query = "SELECT * FROM usuarios;";
-    con.query(sql_query, function(err, result){
-        if (err) throw err;
-        for(let i=0; i<result.length; i++){
-            console.log(result[i].name);
-        }
-        res.json(result);
-    });
-    //res.send("Hello world");
-});
+// USER END POINT
+// GET /users
+app.get("/user/all", (req, res) => {
+  let sql_query = "SELECT * FROM users;"
+  con.query(sql_query, function (err, result) {
+    if (err) throw err
+    for (let i = 0; i < result.length; i++) {
+      console.log(result[i].name)
+    }
+    res.json(result)
+  })
+})
+// POST /user/add
+app.post("/user/add", (req, res) => {
+  data = req.body
+  console.log(data)
 
-app.post('/add', (req, res)=>{
-    data = req.body;
-    console.log(data);
+  let sql_query = "INSERT INTO users VALUES(?,?,?,?,?,?);"
+  con.query(
+    sql_query,
+    [data.userid, data.name, data.lastname, data.phone, data.privilege, 0],
+    function (err, results) {
+      if (err) throw err
+      console.log(results.message)
+    }
+  )
+  res.send("User created!")
+})
 
-    let sql_query = "INSERT INTO usuarios VALUES(DEFAULT, ?,?,?);";
-    con.query(sql_query, [data.name, data.lastname, data.phone], function(err, results){
-        if (err) throw err;
-        console.log(results.message);
-    });
+// REPORTS END POINT
+// GET /report
+// Get active reports
+app.get("/report", (req, res) => {
+  let sql_query = "SELECT * FROM `reports` WHERE status = 1; "
+  con.query(sql_query, function (err, result) {
+    if (err) throw err
+    for (let i = 0; i < result.length; i++) {
+      console.log(result[i].title)
+    }
+    res.json(result)
+  })
+})
+// GET /report/all
+// Get all reports
+app.get("/report/all", (req, res) => {
+  let sql_query = "SELECT * FROM `reports`; "
+  con.query(sql_query, function (err, result) {
+    if (err) throw err
+    for (let i = 0; i < result.length; i++) {
+      console.log(result[i].title)
+    }
+    res.json(result)
+  })
+})
+// POST /report/add
+app.post("/report/add", (req, res) => {
+  data = req.body
+  console.log(data)
 
+  let sql_query = "INSERT INTO reports VALUES(?,?,?,?,?,?);"
+  con.query(
+    sql_query,
+    [
+      data.reportid,
+      data.userid,
+      data.date,
+      data.title,
+      data.description,
+      data.status,
+    ],
+    function (err, results) {
+      if (err) throw err
+      console.log(results.message)
+    }
+  )
+  res.send("Report created!")
+})
 
-    res.send("User created!");
-});
+// PORT
+app.listen(5000)
 
-app.listen(5000);
-
-console.log("App running at http://localhost:5000");
+console.log("App running at http://localhost:5000")
